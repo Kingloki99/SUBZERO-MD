e74118e078d58d8b68d4d4993f2a710e1e879b42
Kingloki99 <168367045+Kingloki99@users.noreply.github.com>
2025-09-14 09:21:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;rZl2wSpZ#ASkrPWOhl45ynQeEQboyQjLBIQgAfO4f7ns4xzgGIPE", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;eFMRQAoA#JVFEGochXXgK55_i0f2xZsfCXd1foaCahTKgd2z1gfc", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "2348161433620", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
